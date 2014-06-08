ba1736f4d3fb003d8c5f5cf3f6fb3c1e657be23f
minsky <luckyweijie@gmail.com>
2014-06-08 13:46:23 +0800

    My awesome config, First Commit

---
+++ menu.lua
@@ -0,0 +1,79 @@
+ local menuc8205c7636e728d448c2774e6a4a944b = {
+     {"Avahi SSH服务器浏览器", "/usr/bin/bssh", "/usr/share/icons/gnome/48x48/devices/network-wired.png" },
+     {"Avahi VNC服务器浏览器", "/usr/bin/bvnc", "/usr/share/icons/gnome/48x48/devices/network-wired.png" },
+     {"Firefox", "/usr/lib/firefox/firefox ", "/usr/share/icons/hicolor/48x48/apps/firefox.png" },
+     {"TightVNC Viewer", "vncviewer", "/usr/share/pixmaps/tightvnc.png" },
+     {"Transmission", "transmission-gtk ", "/usr/share/icons/hicolor/48x48/apps/transmission.png" },
+ }
+
+ local menudb53804b7d726967c58fcc8c9ca03d27 = {
+     {"Adobe Reader 9", "acroread ", "/usr/share/pixmaps/acroread.png" },
+     {"KchmViewer", "kchmviewer ", "/usr/share/pixmaps/kchmviewer.png" },
+     {"Okular", "okular   -caption Okular", "/usr/share/icons/hicolor/48x48/apps/okular.png" },
+     {"WPS 文字", "/usr/bin/wps ", "/usr/share/icons/hicolor/48x48/apps/wps-office-wpsmain.png" },
+     {"WPS 演示", "/usr/bin/wpp ", "/usr/share/icons/hicolor/48x48/apps/wps-office-wppmain.png" },
+     {"WPS 表格", "/usr/bin/et ", "/usr/share/icons/hicolor/48x48/apps/wps-office-etmain.png" },
+ }
+
+ local menue1b7cee794666669f55acead93513971 = {
+     {"Dia 图表编辑器", "dia ", "/usr/share/icons/hicolor/48x48/apps/dia.png" },
+     {"GNU 图像处理程序", "gimp-2.8 ", "/usr/share/icons/hicolor/48x48/apps/gimp.png" },
+     {"KolourPaint", "kolourpaint ", "/usr/share/icons/hicolor/48x48/apps/kolourpaint.png" },
+     {"Okular", "okular   -caption Okular", "/usr/share/icons/hicolor/48x48/apps/okular.png" },
+     {"图像查看器", "gpicview ", "/usr/share/icons/hicolor/48x48/apps/gpicview.png" },
+ }
+
+ local menu526fe18cc54d217db3fdb0bf6c5e9bc8 = {
+     {"Audio Mixer", "xfce4-mixer", "/usr/share/icons/hicolor/48x48/apps/multimedia-volume-control.png" },
+     {"Exaile", "exaile ", "/usr/share/pixmaps/exaile.png" },
+     {"Qt V4L2 test Utility", "qv4l2", "/usr/share/icons/hicolor/48x48/apps/qv4l2.png" },
+     {"SMPlayer", "smplayer ", "/usr/share/icons/hicolor/48x48/apps/smplayer.png" },
+     {"gMTP", "gmtp", "/usr/share/pixmaps/gMTPicon.png" },
+ }
+
+ local menude71492af019157e5a5ef958b570e760 = {
+     {"Avahi Zero浏览器", "/usr/bin/avahi-discover", "/usr/share/icons/gnome/48x48/devices/network-wired.png" },
+     {"Fcitx", "fcitx", "/usr/share/icons/hicolor/48x48/apps/fcitx.png" },
+     {"Oracle VM VirtualBox", "VirtualBox ", "/usr/share/icons/hicolor/48x48/mimetypes/virtualbox.png" },
+     {"Thunar 文件管理器", "thunar ", "/usr/share/icons/hicolor/48x48/apps/Thunar.png" },
+     {"UXTerm", "uxterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+     {"XTerm", "xterm", "/usr/share/pixmaps/xterm-color_48x48.xpm" },
+     {"Xfce 终端", "xfce4-terminal", "/usr/share/icons/gnome/48x48/apps/utilities-terminal.png" },
+     {"dconf 系统配置编辑器", "dconf-editor", "/usr/share/icons/hicolor/48x48/apps/dconf-editor.png" },
+     {"任务管理器", "xfce4-taskmanager", "/usr/share/icons/gnome/48x48/apps/utilities-system-monitor.png" },
+     {"传感器查看器", "xfce4-sensors", "/usr/share/icons/hicolor/24x24/apps/xfce-sensors.png" },
+     {"批量重命名", "/usr/lib/Thunar/ThunarBulkRename ", "/usr/share/icons/hicolor/48x48/apps/Thunar.png" },
+ }
+
+ local menu41282bf95e43c64d579757573a03cdde = {
+     {"CMake", "cmake-gui ", "/usr/share/pixmaps/CMakeSetup32.png" },
+     {"Code::Blocks IDE", "codeblocks ", "/usr/share/pixmaps/codeblocks.png" },
+     {"Emacs", "emacs ", "/usr/share/icons/hicolor/48x48/apps/emacs.png" },
+     {"Qt4 Assistant ", "assistant-qt4", "/usr/share/icons/hicolor/32x32/apps/assistant-qt4.png" },
+     {"Qt4 Designer", "designer-qt4", "/usr/share/icons/hicolor/128x128/apps/designer-qt4.png" },
+     {"Qt4 Linguist ", "linguist-qt4", "/usr/share/icons/hicolor/48x48/apps/linguist-qt4.png" },
+     {"Qt4 QDbusViewer ", "qdbusviewer-qt4", "/usr/share/icons/hicolor/32x32/apps/qdbusviewer-qt4.png" },
+     {"wxFormBuilder", "wxformbuilder ", "/usr/share/pixmaps/wxformbuilder.png" },
+ }
+
+ local menuc9a6ee90f5d43732e3f6cf4dcaa8493c = {
+     {"7-Zip FM", "7zFM", "/usr/share/icons/hicolor/32x32/apps/p7zip.png" },
+     {"CHM 阅读器", "gnochm ", "/usr/share/pixmaps/gnochm.png" },
+     {"Mousepad", "mousepad ", "/usr/share/icons/gnome/48x48/apps/accessories-text-editor.png" },
+     {"Sublime Text", "subl ", "/usr/share/icons/hicolor/48x48/apps/sublime_text.png" },
+     {"Vi IMproved", "gvim -f ", "/usr/share/pixmaps/gvim.png" },
+     {"Xarchiver", "xarchiver", "/usr/share/icons/hicolor/48x48/apps/xarchiver.png" },
+     {"应用程序查找器", "xfce4-appfinder", "/usr/share/icons/gnome/48x48/actions/gtk-find.png" },
+     {"截图", "xfce4-screenshooter", "/usr/share/icons/hicolor/48x48/apps/applets-screenshooter.png" },
+ }
+
+xdgmenu = {
+    {"Internet", menuc8205c7636e728d448c2774e6a4a944b},
+    {"办公", menudb53804b7d726967c58fcc8c9ca03d27},
+    {"图像", menue1b7cee794666669f55acead93513971},
+    {"影音", menu526fe18cc54d217db3fdb0bf6c5e9bc8},
+    {"系统工具", menude71492af019157e5a5ef958b570e760},
+    {"编程", menu41282bf95e43c64d579757573a03cdde},
+    {"附件", menuc9a6ee90f5d43732e3f6cf4dcaa8493c},
+}
+
